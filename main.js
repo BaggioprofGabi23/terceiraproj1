25ff04d80ea29e8a62be9ec71f2198eab9d00a80
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-03-20 13:49:22 -0300

    lll

--- main.js
+++ main.js
@@ -28,4 +28,7 @@ function calculaTempo(tempoObjetivo) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 }
