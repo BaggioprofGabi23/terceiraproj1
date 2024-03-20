41438f0f2bac2330ed0e1ffb8f66b460f8f4c9ac
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-03-20 13:51:18 -0300

    kkk

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ let tempoAtual = new Date();
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 function calculaTempo(tempoObjetivo) {
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
@@ -28,7 +31,5 @@ function calculaTempo(tempoObjetivo) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
+
 }
